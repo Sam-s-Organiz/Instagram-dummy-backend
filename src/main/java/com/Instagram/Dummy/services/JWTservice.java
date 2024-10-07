8c58a8d75b3318c16413082a8a2dab4b276d7413
Extremis0 <tanmaymadhure1@gmail.com>
2024-10-07 11:17:33 +0530

    jwt modified

--- src/main/java/com/Instagram/Dummy/services/JWTservice.java
+++ src/main/java/com/Instagram/Dummy/services/JWTservice.java
@@ -1,8 +1,10 @@
 package com.Instagram.Dummy.services;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
@@ -10,6 +12,7 @@ import javax.crypto.SecretKey;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
+import java.util.function.Function;
 
 @Service
 public class JWTservice {
@@ -39,8 +42,37 @@ public class JWTservice {
                 .compact();
     }
 
-    private Key getKey() {
+    private SecretKey getKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKet);
         return Keys.hmacShaKeyFor(keyBytes);
     }
+
+    public String extractUserName(String token) {
+        return extractClaims(token, Claims::getSubject);
+    }
+    private  <T> T extractClaims(String token, Function<Claims,T> claimResolver){
+        final Claims claims =extractallClaims(token);
+        return claimResolver.apply(claims);
+    }
+    private Claims extractallClaims(String token){
+        return Jwts
+                .parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username=extractUserName(token);
+        return (username.equals(userDetails.getUsername()))&&!isTokenExpired(token);
+    }
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+    private Date extractExpiration(String token) {
+        return extractClaims(token,Claims::getExpiration);
+    }
+
 }
