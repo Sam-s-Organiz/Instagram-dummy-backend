a9ed4cb40a584af2e6b7c5eba7428a48a1772a44
Saurabh_Madhure <saurabh.madhure@nextuple.com>
2024-12-26 09:55:47 +0530

    added search api for userSearch

--- src/main/java/com/Instagram/Dummy/repo/UserRepository.java
+++ src/main/java/com/Instagram/Dummy/repo/UserRepository.java
@@ -1,8 +1,12 @@
 package com.Instagram.Dummy.repo;
 
 import com.Instagram.Dummy.modals.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -10,4 +14,12 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
+    @Query(value = """
+                SELECT u 
+                FROM User u 
+                WHERE 
+                    LOWER(u.username) LIKE LOWER(CONCAT('%', :term, '%')) 
+                    OR LOWER(u.email) LIKE LOWER(CONCAT('%', :term, '%'))
+            """)
+    Page<User> findUsersByUsernameOrEmailContaining(@Param("term") String term, Pageable pageable);
 }
